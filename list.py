1f88f095a069ac6f590473f221596a8588d11b3f
natheen469 <hariniva12345@gmail.com>
2026-05-05 12:47:33 +0530

    Second-commit

--- list.py
+++ list.py
@@ -176,22 +176,22 @@
 
 
 
-# def is_strong_password(password):
-#     # Conditions
-#     if (len(password) >= 8 and
-#         (c.isupper() for c in password) and
-#         (c.islower() for c in password) and
-#         (c.isdigit() for c in password) and
-#         (c in "@#$%&!" for c in password)):
-#         return True
-#     return False
+def is_strong_password(password):
+    # Conditions
+    if (len(password) >= 8 and
+        (c.isupper() for c in password) and
+        (c.islower() for c in password) and
+        (c.isdigit() for c in password) and
+        (c in "@#$%&!" for c in password)):
+        return True
+    return False
 
 
-# # User input
-# pwd = input("Enter your password: ")
+# User input
+pwd = input("Enter your password: ")
 
-# # Check password
-# if is_strong_password(pwd):
-#     print(" Strong password")
-# else:
-#     print(" Weak password - Please create a strong password")
+# Check password
+if is_strong_password(pwd):
+    print(" Strong password")
+else:
+    print(" Weak password - Please create a strong password")
